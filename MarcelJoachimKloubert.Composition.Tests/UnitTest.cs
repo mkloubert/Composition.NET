c32f62e488fb1f860a6aba53e16043b0924c35f7
Marcel Joachim Kloubert <marcel.kloubert@gmx.net>
2015-10-31 01:06:40 +0100

    - ADD: test project

---
+++ MarcelJoachimKloubert.Composition.Tests/UnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System.Composition.Hosting;
+using System.Reflection;
+
+namespace MarcelJoachimKloubert.Composition.Tests
+{
+    
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
